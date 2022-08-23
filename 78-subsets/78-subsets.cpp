3cd99d05acca327780f13bf22cf40cb738102a4b
NAMAN KESHERWANI <86315451+NamannNK@users.noreply.github.com>
2022-08-23 20:53:25 +0530

    Time: 10 ms (17.23%), Space: 16.8 MB (13.83%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void solve(int i,vector<int>nums,vector<vector<int>>&res,vector<int>v){
+        if(i==nums.size()){
+            res.push_back(v);
+            return ;
+        }
+        v.push_back(nums[i]);
+        solve(i+1,nums,res,v);
+        v.pop_back();
+        solve(i+1,nums,res,v);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>res;
+        vector<int>v;
+        solve(0,nums,res,v);
+        return res;
+    }
+};
