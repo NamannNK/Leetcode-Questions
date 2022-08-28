73ff555f4f971c40ec6b6e43bfaea934ffab426a
NAMAN KESHERWANI <86315451+NamannNK@users.noreply.github.com>
2022-08-28 20:26:11 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (57.44%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int climbStairs(int n) {
         if(n<=2)return n;
-        vector<int>dp(n+1,-1);
-        dp[1]=1;
-        dp[2]=2;
+        int last=2;
+        int secondlast=1;
         for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int x= last+secondlast;
+            secondlast = last;
+            last=x;
         }
-        return dp[n];
+        return last;
     }
 };
