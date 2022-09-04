80041729b4e6d6f473bac641c95c8cb818b07719
NAMAN KESHERWANI <86315451+NamannNK@users.noreply.github.com>
2022-09-04 14:13:30 +0530

    Time: 11 ms (39.72%), Space: 12.1 MB (84.68%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,34 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        if(root == NULL)
-            return {};
-        queue<pair<TreeNode*,pair<int,int>>>q;
-        q.push({root,{0,0}});
-        map<int,map<int,multiset<int>>>m;
+        queue<pair<TreeNode*,int>>q;
+        if(root==NULL)return {};
+        q.push({root,0});
+        map<int,vector<int>>m;
         while(q.empty() == false){
-            TreeNode* temp = q.front().first;
-            int level = q.front().second.second;
-            int vertical = q.front().second.first;
-            q.pop();
-            m[vertical][level].insert(temp->val);
-            if(temp->left != NULL){
-                q.push({temp->left,{vertical-1,level+1}});
-            }
-            if(temp->right != NULL){
-                q.push({temp->right,{vertical+1,level+1}});
-            }
-        }
-        vector<vector<int>>res;
-        for(auto x:m){
-            vector<int>v;
-            for(auto y:x.second){
-                for(auto s: y.second){
-                    v.push_back(s);
+            int n=q.size();
+            map<int,vector<int>>mt;
+            for(int i=0;i<n;i++){
+                TreeNode* temp=q.front().first;
+                int col=q.front().second;
+                q.pop();
+                mt[col].push_back(temp->val);
+                if(temp->left!=NULL){
+                    q.push({temp->left,col-1});
+                }
+                if(temp->right!=NULL){
+                    q.push({temp->right,col+1});
                 }
             }
-            res.push_back(v);
+            for(auto x:mt){
+                int i=x.first;
+                vector<int>v = x.second;
+                sort(v.begin(),v.end());
+                for(auto y:v){
+                    m[i].push_back(y);
+                }
+            }
         }
-        return res;
+        vector<vector<int>>Res;
+        for(auto x:m){
+            Res.push_back(x.second);
+        }
+        return Res;
     }
 };
